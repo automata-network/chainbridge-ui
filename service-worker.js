c04d0accae88d740f7247b50f861a95faf514b3b
RyuH1 <79819095+RyuH1@users.noreply.github.com>
2021-10-11 09:45:04 +0000

    Deploying to gh-pages from @ automata-network/chainbridge-ui@434b25a19bcb4307fc39c104ee7e944d601d7541 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6be8b5fc6c221d5654e6443e72373dab.js"
+  "/chainbridge-ui/precache-manifest.dc09314e74afc88c3e9616eadf074fba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chainbridge-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
