be7911038e46e74adcedab2dfaa340dc34d3dfc5
duxiaofeng-github <duxiaofeng-github@users.noreply.github.com>
2022-02-04 08:01:09 +0000

    Deploying to gh-pages from @ automata-network/chainbridge-ui@28423859436436e59c9835768fd77a86d6fa7e4d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.44e5aa7b9130d82d909f4a62ba4e60f0.js"
+  "./precache-manifest.3cde0dba4ae1dd12d64c140946f9d074.js"
 );
 
 self.addEventListener('message', (event) => {
